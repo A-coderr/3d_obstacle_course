d9366b7745d86f1dbc43917fb1b714e14dca3524
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-12 13:00:43 -0400

    Ensured the Start screen doesn't show up before the loading of the game is complete.

--- src/App.tsx
+++ src/App.tsx
@@ -5,39 +5,33 @@ import "./App.css";
 import { Suspense } from "react";
 import Loader from "./components/UI/Loader";
 import { Physics } from "@react-three/rapier";
-import { Environment, KeyboardControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
 import StartScreen from "./components/UI/StartScreen";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
 
 const HDR_PATH = new URL("./assets/sky_1k.hdr", import.meta.url).href;
 
-const keyboardMap = [
-  { name: "forward", keys: ["ArrowUp", "KeyW"] },
-  { name: "backward", keys: ["ArrowDown", "KeyS"] },
-  { name: "left", keys: ["ArrowLeft", "KeyA"] },
-  { name: "right", keys: ["ArrowRight", "KeyD"] },
-  { name: "run", keys: ["Shift"] },
-];
-
-export default function Scene() {
+function App() {
+  const { isLoading } = useSelector((state: RootState) => state.game);
   return (
     <>
-      <KeyboardControls map={keyboardMap}>
-        <Canvas
-          resize={{ polyfill: ResizeObserver }}
-          camera={{ position: [3, 8, 3], near: 0.3, fov: 40 }}
-        >
-          <Suspense fallback={<Loader />}>
-            <gridHelper args={[2000, 2000, 0xff22aa, 0x55ccff]} />
-            {/* <Perf position="top-left" /> */}
-            <Environment files={HDR_PATH} background />
-            <Physics debug>
-              <Experience />
-            </Physics>
-          </Suspense>
-        </Canvas>
-      </KeyboardControls>
+      <Canvas
+        resize={{ polyfill: ResizeObserver }}
+        camera={{ position: [3, 8, 3], near: 0.3, fov: 40 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <gridHelper args={[2000, 2000, 0xff22aa, 0x55ccff]} />
+          {/* <Perf position="top-left" /> */}
+          <Environment files={HDR_PATH} background />
+          <Physics debug>
+            <Experience />
+          </Physics>
+        </Suspense>
+      </Canvas>
 
-      <StartScreen />
+      {!isLoading && <StartScreen />}
     </>
   );
 }
+export default App;
--- src/components/UI/Loader.tsx
+++ src/components/UI/Loader.tsx
@@ -1,11 +1,21 @@
 import { useProgress, Html } from "@react-three/drei";
 import { BeatLoader } from "react-spinners";
+import { setLoadingComplete } from "../../store/gameSlice";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 /**
  * Loader component serves as a loading indicator using
  * React Spinners and R3F's useProgress hook to display the loading progress.
  */
 const Loader = () => {
   const { progress } = useProgress();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (progress === 100) {
+      dispatch(setLoadingComplete());
+    }
+  }, [progress, dispatch]);
 
   return (
     // Html is used to render DOM elements inside the 3D canvas to avoid R3F errors.
