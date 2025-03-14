72d1931b0dfc11279d411afcb17a4b2e5efb81bf
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-14 16:45:29 -0400

    Further updates to the start screen.

--- src/components/UI/StartScreen.tsx
+++ src/components/UI/StartScreen.tsx
@@ -41,14 +41,17 @@ const StartScreen = () => {
 
           {/* Right Half: Menu */}
           <div className="w-1/2 h-screen flex items-center justify-center">
-            <div className="bg-[#000025] text-white py-10 border-2 border-pink-500 rounded-lg shadow-[0_0_15px_#ff00ff] flex flex-col items-center max-w-2xl w-full select-none">
+            <div className="bg-[#000025] text-white py-10 px-2 border-2 border-pink-500 rounded-lg shadow-[0_0_15px_#ff00ff] flex flex-col items-center max-w-2xl w-full select-none">
               <h1 className="text-5xl font-bold">ThreeRun</h1>
-              <div className="flex flex-row items-center gap-3 w-full pt-5">
-                <div className="flex flex-col items-center gap-3 w-2/3">
+              <div className="flex flex-row items-center gap-20 mt-20">
+                <div className="w-1/4 text-right">
+                  <h2 className="text-2xl uppercase font-semibold">Move</h2>
+                </div>
+                <div className="flex flex-col items-center gap-3 w-1/2">
                   <kbd className="min-h-7.5 inline-flex justify-center items-center py-2 px-4 bg-pink-200 border-2 border-pink-500 shadow-[0_0_15px_#ff00ff] font-mono text-xl font-bold text-gray-800 rounded-md">
                     W
                   </kbd>
-                  <div className="flex items-center justify-between w-1/2">
+                  <div className="flex flex-row items-center gap-6 w-full">
                     <kbd className="py-2 px-4 bg-pink-200 border-2 border-pink-500 shadow-[0_0_15px_#ff00ff] font-mono text-xl font-bold text-gray-800 rounded-md">
                       A
                     </kbd>
@@ -58,33 +61,30 @@ const StartScreen = () => {
                     </kbd>
                   </div>
                 </div>
-                <div className="w-1/3">
-                  <h2 className="text-2xl">- To move</h2>
-                </div>
               </div>
               <div className="flex flex-row items-center gap-3 w-full pt-5">
-                <div className="flex flex-col items-center gap-3 w-2/3">
+                <div className="w-1/4 text-right">
+                  <h2 className="text-2xl uppercase font-semibold">Run</h2>
+                </div>
+                <div className="flex flex-col items-center gap-3 w-1/2">
                   <kbd className="min-h-7.5 inline-flex justify-center items-center py-2 px-4 bg-pink-200 border-2 border-pink-500 shadow-[0_0_15px_#ff00ff] font-mono text-xl font-bold text-gray-800 rounded-md">
                     Shift
                   </kbd>
                 </div>
-                <div className="w-1/3">
-                  <h2 className="text-2xl">- To run</h2>
-                </div>
               </div>
               <div className="flex flex-row items-center gap-3 w-full pt-5">
-                <div className="flex flex-col items-center gap-3 w-2/3">
+                <div className="w-1/4 text-right">
+                  <h2 className="text-2xl uppercase font-semibold">Jump</h2>
+                </div>
+                <div className="flex flex-col items-center gap-3 w-1/2">
                   <kbd className="min-h-7.5 inline-flex justify-center items-center py-2 px-15 bg-pink-200 border-2 border-pink-500 shadow-[0_0_15px_#ff00ff] font-mono text-xl font-bold text-gray-800 rounded-md">
                     Space
                   </kbd>
                 </div>
-                <div className="w-1/3">
-                  <h2 className="text-2xl">- To jump</h2>
-                </div>
               </div>
 
               <button
-                className="mt-10 px-10 py-4 text-white font-bold uppercase bg-black border-2 border-purple-500 rounded-lg shadow-[0_0_15px_#a855f7] hover:shadow-[0_0_25px_#a855f7] transition duration-300"
+                className="mt-20 px-10 py-4 text-white font-bold uppercase bg-black border-2 border-purple-500 rounded-lg shadow-[0_0_15px_#a855f7] hover:shadow-[0_0_25px_#a855f7] transition duration-300"
                 onClick={() => {
                   dispatch(startGame());
                   setShowWelcome(false);
