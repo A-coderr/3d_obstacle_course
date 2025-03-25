77d399de011d3fcb68a424abbbd5396801574d3e
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-03-25 19:58:34 -0400

    Removed unused props

--- src/App.tsx
+++ src/App.tsx
@@ -14,12 +14,10 @@ import GameTimer from "./components/UI/GameTimer";
 const HDR_PATH = new URL("./assets/sky.hdr", import.meta.url).href;
 
 function App() {
-  const { isLoading, isGameStarted } = useSelector(
-    (state: RootState) => state.game
-  );
+  const { isLoading } = useSelector((state: RootState) => state.game);
   return (
     <>
-      {!isLoading && <GameTimer isGameStarted={isGameStarted} />}
+      {!isLoading && <GameTimer />}
       <Canvas
         resize={{ polyfill: ResizeObserver }}
         camera={{ position: [3, 8, 3], near: 0.3, fov: 40 }}
