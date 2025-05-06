065f98933223de90b0446ad7866fe0703f40a402
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2025-05-06 10:58:36 -0400

    Ensured the pause button is not focusable with the tab.

--- src/components/UI/PauseButton.tsx
+++ src/components/UI/PauseButton.tsx
@@ -19,6 +19,7 @@ const PauseButton = () => {
       className="absolute z-10 top-5 right-5 px-5 py-3 text-[#00a2ff] font-bold text-2xl border-4 border-[#00a2ff] 
                  rounded-full bg-black shadow-[0_0_15px_#00a2ff] transition-all duration-300 
                  hover:shadow-[0_0_25px_#00a2ff] hover:scale-110 active:scale-100 select-none"
+      tabIndex={-1}
     >
       II
     </button>
